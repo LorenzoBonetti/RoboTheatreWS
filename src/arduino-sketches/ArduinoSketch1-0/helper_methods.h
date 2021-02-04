4d111b5dd893e375da80db1217ca5c467688c77f
LorenzoBonetti <lorenzo2.bonetti@mail.polimi.it>
2021-02-04 14:07:40 +0100

    bug fix arduino

--- src/arduino-sketches/ArduinoSketch1-0/helper_methods.h
+++ src/arduino-sketches/ArduinoSketch1-0/helper_methods.h
@@ -2,4 +2,4 @@
 #include <Arduino.h>
 int move_servo_by_one_degree(Servo&servo, int final_angle);
 void writeServo(Servo &theServo, int angle);
-float getVoltageLevel(int battery_pin, int low_battery_voltage, int voltage_th);
+float getVoltageLevel(int battery_pin, int low_battery_voltage, float voltage_th);
